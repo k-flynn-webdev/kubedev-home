5bf0d2f42d06e04475018497f82932b5fe9334a2
kevinflynn <flynny85@gmail.com>
2024-05-14 14:49:40 +0100

    updated skill

--- src/components/about-skills.js
+++ src/components/about-skills.js
@@ -1,26 +1,37 @@
-import * as React from "react"
-import Skills from "./skills"
+import * as React from "react";
+import Skills from "./skills";
 
-const languages = ['javascript', 'python', 'c# (Unity)']
-const tools = ['vue', 'react', 'node',  'express', 'django', 'nginx', 'unity', 'docker']
-const test = ['jest', 'gherkin',  'cypress']
-const general = ['web', 'animation',  'games']
+const languages = ["javascript", "python", "c# (Unity)"];
+const tools = [
+  "nuxt",
+  "vue",
+  "react",
+  "node",
+  "express",
+  "django",
+  "nginx",
+  "unity",
+  "docker",
+];
+const test = ["jest", "gherkin", "cypress"];
+const general = ["web", "animation", "games"];
 
-const details = [languages, tools, test, general]
-
+const details = [languages, tools, test, general];
 
 const About = ({ className }) => {
   return (
-		<div className={`skills mb-5 mb-md-6 ${className}`}>
+    <div className={`skills mb-5 mb-md-6 ${className}`}>
+      <div className="details">
+        {details.map((detail, idx) => (
+          <Skills
+            key={idx}
+            listName={idx}
+            list={detail}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-				<div className="details">
-					{details.map((detail, idx) =>
-							<Skills key={idx} listName={idx} list={detail}/>
-					)}
-				</div>
-
-		</div>
-  )
-}
-
-export default About
+export default About;
